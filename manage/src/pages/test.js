db6272244cef34aa002edc02cbef264969d70a26
MKWqq <wangqingqing@axinfu.com>
2019-11-20 15:10:14 +0800

    '环境搭建'

---
+++ manage/src/pages/test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Button} from 'antd'
+import 'antd/lib'
+
+export default class Test extends React.Component{
+	render(){
+		return (
+			<div>
+				<Button>123</Button>
+			</div>
+		)
+	}
+};
