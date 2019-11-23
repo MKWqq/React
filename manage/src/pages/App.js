8a98fe0a0e8106fffde6cbe33205f4433fb3b48a
MKWqq <wangqingqing@axinfu.com>
2019-11-23 17:31:48 +0800

    'nav'

---
+++ manage/src/pages/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from 'components/Header'
+import NavLeft from 'components/NavLeft'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <NavLeft></NavLeft>
+    </div>
+  );
+}
+
+export default App;
