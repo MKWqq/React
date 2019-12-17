1c1a92c5e2f9d34d48cb952fdb10e81e58253be0
王清清 <wangqingqing@axinfu.com>
2019-12-17 22:18:02 +0800

    update

--- manage/client/src/pages/main/systemSetting/component/EditableFormTable.js
+++ manage/client/src/pages/main/systemSetting/component/EditableFormTable.js
@@ -1,156 +1,87 @@
 /* 可编辑table组件——基于antd组件 */
-import React, {Component} from 'react'
-import {Table, Tag, Input, InputNumber, Popconfirm, Form} from 'antd'
+import React, {Component} from 'react';
+import {Form, Table, Tag} from 'antd';
 
 /* 模拟数据 */
 const mockRespData = [
-	{storeName: '春熙路', address: '春熙路110号', telephone: '15156278767', status: '0'},
-	{storeName: '春熙路', address: '春熙路110号', telephone: '15156278767', status: '1'},
-	{storeName: '春熙路', address: '春熙路110号', telephone: '15156278767', status: '1'},
-	{storeName: '春熙路', address: '春熙路110号', telephone: '15156278767', status: '0'},
+    {storeName: '春熙路', address: '春熙路110号', telephone: '15156278767', status: '0'},
+    {storeName: '春熙路', address: '春熙路110号', telephone: '15156278767', status: '1'},
+    {storeName: '春熙路', address: '春熙路110号', telephone: '15156278767', status: '1'},
+    {storeName: '春熙路', address: '春熙路110号', telephone: '15156278767', status: '0'}
 ];
 
 const data = [];
 
 mockRespData.forEach((item, index) => {
-	data.push(
-		{
-			key: index.toString(),
-			storeName: item.storeName,
-			address: item.address,
-			telephone: item.telephone,
-			status: item.status
-		}
-	);
+    data.push(
+        {
+            key: index.toString(),
+            storeName: item.storeName,
+            address: item.address,
+            telephone: item.telephone,
+            status: item.status
+        }
+    );
 });
 
 const EditableContext = React.createContext({});
 
-class EditableCell extends Component {
-	getInput = () => {
-		if (this.props.inputType === 'number') {
-			return <InputNumber/>;
-		}
-		return <Input/>;
-	};
-	/* 可编辑input */
-	renderCell = ({getFieldDecorator}) => {
-		const {editing, dataIndex, title, inputType, record, index, children, ...restProps} = this.props;
-		return (
-			<td {...restProps}>
-				{editing ? (
-					<Form.Item style={{margin: 0}}>
-						{getFieldDecorator(dataIndex, {
-							rules: [],
-							initialValue: record[dataIndex]
-						})(this.getInput())}
-					</Form.Item>
-				) : (
-					children
-				)}
-			</td>
-		);
-	};
-
-	render() {
-		return <EditableContext.Consumer>{this.renderCell}</EditableContext.Consumer>
-	}
-}
-
 class EditableTable extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {data, editingKey: ''};
-		this.columns = [
-			{title: '门店名', dataIndex: 'storeName', editable: true},
-			{title: '地址', dataIndex: 'address', editable: true},
-			{title: '联系电话', dataIndex: 'telephone', editable: true},
-			{title: '配送状态', dataIndex: 'status', editable: true},
-			{
-				title: '操作', dataIndex: 'operation', render: (text, record) => {
-					const {editingKey} = this.state;
-					const editable = this.isEditing(record);
-					return editable ? (
-						<span>
-							<EditableContext.Consumer>
-								{form => (
-									<a onClick={() => this.save(form, record.key)} style={{marginRight: 8}}>
-										保存
-									</a>
-								)}
-							</EditableContext.Consumer>
-							<Popconfirm title='确认取消？' onClick={() => this.edit(record.key)}>
-								<a>取消</a>
-							</Popconfirm>
-						</span>
-					) : (
-						<a disabled={editingKey !== ''} onClick={() => this.edit(record.key)}>编辑</a>
-					)
-				}
-			}
-		];
-	}
+    constructor(props) {
+        super(props);
+        this.columns = [
+            {title: '门店名', dataIndex: 'storeName', editable: true},
+            {title: '地址', dataIndex: 'address', editable: true},
+            {title: '联系电话', dataIndex: 'telephone', editable: true},
+            {
+                title: '配送状态', dataIndex: 'status', editable: true, render: (currentStatus, columnItem) => {
+                    return (
+                        <Tag
+                            color={currentStatus === '0' ? 'volcano' : 'green'}>{currentStatus === '0' ? '停送' : '配送中'}</Tag>
+                    );
+                }
+            },
+            {
+                title: '操作', dataIndex: 'operator', render: () => {
+                    return (
+                        <a>编辑</a>
+                    );
+                }
+            }
+        ];
+    }
 
-	isEditing = record => record.key === this.state.editingKey;
+    cancel = () => {
+    };
 
-	cancel = () => {
-		this.setState({editingKey: ''});
-	};
+    render() {
+        /* waiting */
+        const components={};
 
-	save(form, key) {
-		form.validateFields((err, row) => {
-			if (err) {
-				return false;
-			}
-			const newData = [...this.state.data];
-			const index = newData.findIndex(item => key === item.key);
-			if (index > -1) {
-				const item = newData[index];
-				newData.splice(index, 1, {...item, ...row});
-				this.setState({data: newData, editingKey: ''});
-			} else {
-				newData.push(row);
-				this.setState({data: newData, editingKey: ''});
-			}
-		});
-	};
-
-	edit(key) {
-		this.setState({editingKey: key});
-	}
-
-	render() {
-		const components = {
-			body: {
-				cell: EditableCell,
-			}
-		};
-
-		const columns = this.columns.map(col => {
-			if (!col.editable) {
-				return col;
-			}
-			return {
-				...col,
-				onCell: record => ({
-					record,
-					inputType: col.dataIndex === 'age' ? 'number' : 'text',
-					dataIndex: col.dataIndex,
-					title: col.title,
-					editing: this.isEditing(record)
-				})
-			};
-		});
-
-		return (
-			<EditableContext.Provider value={this.props.form}>
-				<Table components={components} bordered dataSource={this.state.data} columns={columns}
-				       rowClassName='editable-row' pagination={{
-				       	onChange:this.cancel
-				}} />
-			</EditableContext.Provider>
-		);
-	}
+        const columns = this.columns.map((col, index) => {
+            if (!col.editable) {
+                return col;
+            }
+            return {
+                ...col,
+                /* 设置单元格属性 */
+                onCell: (currentRowData) => {
+                    // currentRowData为绑定row的数据
+                    return {
+                        ...currentRowData,
+                        cellType: col.dataIndex === 'status' ? 'select' : 'input'
+                    };
+                }
+            };
+        });
+        return (
+            <EditableContext.Provider value={this.props.form}>
+                <Table components={components} bordered dataSource={data} columns={columns} rowClassName='editable-row' pagination={
+                    {onChange: () => this.cancel}
+                }/>
+            </EditableContext.Provider>
+        );
+    }
 }
 
-export default Form.create({})(EditableTable);
+export default Form.create()(EditableTable);
