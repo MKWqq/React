153be7ad6961fb97fcaa5b993a65bd5deed83b9b
MKWqq <wangqingqing@axinfu.com>
2020-01-13 16:27:34 +0800

    'update'

--- manage/client/src/pages/main/MainPage/MainPage.js
+++ manage/client/src/pages/main/MainPage/MainPage.js
@@ -16,7 +16,9 @@ export default class MainPage extends React.Component{
 
 	render(){
 		return (
-			<div>this is main page</div>
+			<div>
+				this is main page
+			</div>
 		);
 	}
 };
--- manage/server/database/index.js
+++ manage/server/database/index.js
@@ -1,8 +1,11 @@
 /* MongoDB */
 let MongoClient=require('mongodb').MongoClient;
-let url='mongodb://localhost:27017/runoob';
-MongoClient.connect(url,{useNewUrlParser:true},function(err,db){
-	if(err) throw err;
+let url='mongodb://adminRoot:0@192.168.0.225:8000/runoob';
+MongoClient.connect(url,{authSource:'admin',useNewUrlParser:true},function(err,db){
+	if(err){
+		console.log('mongodb服务端连接错误！');
+		throw err;
+	}
 	console.log('数据库已创建');
 
 	let dbase=db.db('runoob');
