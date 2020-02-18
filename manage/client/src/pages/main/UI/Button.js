9d6962d59e96f5871bdd70ab9967d8aef4ab4ed0
MKWqq <wangqingqing@axinfu.com>
2020-02-18 13:57:29 +0800

    'button使用'

--- manage/client/src/pages/main/UI/Button.js
+++ manage/client/src/pages/main/UI/Button.js
@@ -1,38 +1,59 @@
 // Button页面
 import React,{Component} from 'react'
-import {Button,Card} from 'antd'
+import {Button,Card,Radio} from 'antd'
 import '../../less/UI/button.less'
 
 export default class UIButton extends Component{
 	constructor(props){
 		super(props);
+		this.state={
+			size:'large'
+		}
+	}
+
+	changeSize(e){
+		console.log(e.target.value);
+		this.setState({
+			size:e.target.value
+		});
 	}
 
 	render(){
 		return (
 			<div className='g-ui-button'>
-				<div className='clear-float-table' style={{border:'1px solid red'}}>
-					<div className='float-left'>133</div>
-				</div>
-				<div className='g-ui-button-part1'>
-					<header className='h3 padding-bottom-10 border-bottom-gray'>基础按钮</header>
-					<section className='padding-top-10'>
-						<Button className='margin-right-10' type='primary'>primary</Button>
-						<Button className='margin-right-10'>default</Button>
-						<Button className='margin-right-10' type='dashed'>dashed</Button>
-						<Button className='margin-right-10' type="danger">danger</Button>
-						<Button className='margin-right-10' disabled>disabled</Button>
-					</section>
-				</div>
 				<Card title='基础按钮'>
-					<Button className='margin-right-10' type='primary'>primary</Button>
+					<Button type='primary'>primary</Button>
 					<Button className='margin-right-10'>default</Button>
-					<Button className='margin-right-10' type='dashed'>dashed</Button>
-					<Button className='margin-right-10' type="danger">danger</Button>
-					<Button className='margin-right-10' disabled>disabled</Button>
+					<Button type='dashed'>dashed</Button>
+					<Button type="danger">danger</Button>
+					<Button disabled>disabled</Button>
 				</Card>
 				<Card className='margin-top-10' title='图形按钮'>
-					<Button>test</Button>
+					<Button icon="plus">创建</Button>
+					<Button icon="edit">编辑</Button>
+					<Button icon="delete">删除</Button>
+					<Button shape="circle" icon="search" />
+					<Button type='primary' icon='search'>搜索</Button>
+					<Button type="primary" icon="download">下载</Button>
+				</Card>
+				<Card className="margin-top-10" title="Loading按钮">
+					<Button type='primary' icon="loading">确定</Button>
+					<Button type='primary' shape="circle" icon="loading" />
+					<Button icon="loading">点击加载</Button>
+					<Button icon="loading">确定</Button>
+					<Button shape="circle" icon="loading" />
+					<Button type="primary">关闭</Button>
+				</Card>
+				<Card className="margin-top-10" title="按钮组及按钮大小">
+					<Radio.Group value={this.state.size} onChange={(e)=>this.changeSize(e)}>
+						<Radio value="large">大</Radio>
+						<Radio value="default">中</Radio>
+						<Radio value="small">小</Radio>
+					</Radio.Group>
+					<Button.Group size={this.state.size}>
+						<Button type="primary" icon="left">返回</Button>
+						<Button type="primary" icon="right">前进</Button>
+					</Button.Group>
 				</Card>
 			</div>
 		);
